ca6f7d944ade53723336692385bb5eb53593a907
Julio Gutierrez <juliogutepimen@gmail.com>
2018-01-22 23:58:09 -0600

    Hackaton app/With a team of 4

---
+++ Deep watcher/ViewControllerMain.swift
@@ -0,0 +1,27 @@
+//
+//  ViewControllerMain.swift
+//  Deep watcher
+//
+//  Created by Julio Cesar Gutierrez Briones on 27/08/17.
+//  Copyright © 2017 Julio Cesar Gutierrez Briones. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ViewControllerMain: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
